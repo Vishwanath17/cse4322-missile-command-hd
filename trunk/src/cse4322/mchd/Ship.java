5b899c43cbed1c5108084aa44fb38def446d9cab
shawngilleland@gmail.com <shawngilleland@gmail.com@8dff26b4-d14b-89d7-4370-7e2b6005411e>
2012-11-19 05:40:09 +0000

    Began work on adding the city and city missiles.  Added feature to fire missiles from the city but it's either not getting the right angle or it's not updating correctly.

--- trunk/src/cse4322/mchd/Ship.java
+++ trunk/src/cse4322/mchd/Ship.java
@@ -1,85 +1,13 @@
 package cse4322.mchd;
 
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
-public class Ship {
+
+public interface Ship {
 	
-	private Bitmap bitmap; //the actual bitmap
-	private int x;	//the x coordinate
-	private int y;	//the y coordinate
-	private boolean touched;	//if the ship is touched/picked up
 	
-	public Ship(Bitmap bitmap, int x, int y)
-	{
-		this.bitmap = bitmap;
-		this.x = x;
-		this.y = y;
-	}
+	public void update();
 	
-	public Bitmap getBitmap()
-	{
-		return bitmap;
-	}
-	
-	public void setBitmap(Bitmap bitmap)
-	{
-		this.bitmap = bitmap;
-	}
-	
-	public int getX()
-	{
-		return x;
-	}
-	
-	public void setX(int x)
-	{
-		this.x = x;
-	}
-	
-	public int getY()
-	{
-		return y;
-	}
-	
-	public void setY(int y)
-	{
-		this.y = y;
-	}
-	
-	public boolean isTouched()
-	{
-		return touched;
-	}
-	
-	public void setTouched(boolean touched)
-	{
-		this.touched = touched;
-	}
-	
-	public void draw(Canvas canvas)
-	{
-		canvas.drawBitmap(bitmap, x - (bitmap.getWidth()/2), y - (bitmap.getHeight()/2), null);
-	}
-	
-	public void handleActionDown(int eventX, int eventY)
-	{
-		if(eventX >= (x - bitmap.getWidth()/2) && (eventX <= (x + bitmap.getWidth()/2)))
-		{
-			if(eventY >= (y - bitmap.getHeight()/2) && (y <= (y + bitmap.getHeight()/2)))
-			{
-				//ship touched
-				setTouched(true);
-			}
-			else
-			{
-				setTouched(false);
-			}
-		}
-		else
-		{
-			setTouched(false);
-		}
-	}
+	public void draw(Canvas canvas);
 
 }
